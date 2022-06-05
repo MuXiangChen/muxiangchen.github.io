f0093579f2451a66a641c7e46c50d1f36cdc2c0c
Jasmine Mo <loxluthor@gmail.com>
2022-06-06 00:49:22 +0800

    `1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,61 @@
+const { defaultTheme } = require("vuepress");
+
+module.exports = {
+    base: '/3D-Reconstruction-Doc/',
+    head: [['link', { rel: 'icon', href: '/images/heroicon.png' }]],
+    markdown: {
+      lineNumbers:true,
+      extendMarkdown: md => {
+        md.use(require('markdown-it-task-lists'))
+      }
+    },
+    title: '穆相臣的博客',
+    theme: defaultTheme({
+        logo: '/images/heroicon.png',
+        navbar:[
+            // NavbarItem
+            {
+                text: '技术',
+                link: '/TechDoc/',
+            },
+            {
+              text: '设计',
+              link: '/Design/',
+            },
+            {
+                text: '产品',
+                link: '/Products/',
+            },'/README.md'
+        ],
+        sidebar: {
+            '/TechDoc/': [
+              {
+                text: '技术',
+                children: [
+                    '/TechDoc/slam.md', 
+                    '/TechDoc/vuepress2.md', 
+                ],
+              },
+            ],
+            '/Design/': [
+              {
+                text: '设计',
+                children: [
+                    '/Design/README.md', 
+                ],
+              },
+            ],
+            '/Products/': [
+              {
+                text: '产品',
+                children: [
+                  '/Design/README.md', 
+                ],
+              },
+            ],
+              
+            
+          },
+
+    })
+  }
